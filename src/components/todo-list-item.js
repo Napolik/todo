75f230778b9c5fe7de30026a277e7ad05785273f
Napolik <napolik@MacBook-MacBook.Dlink>
2021-05-17 16:22:44 +0300

    Imported css styles

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import './todo-list-item.css';
 
 const TodoListItem = ( {label, important = false} ) => {
   const style = {
     color: important ? 'tomato' : 'black' 
   };
     return (
-        <span style={style}>{ label }</span>
+        <div>
+        <span 
+          className="todo-list-item" 
+          style={style}>
+          { label }
+        </span>
+
+        <button type="button" className="btn btn-outline-success btn-small">
+          <i className="fa fa-exclametion" />
+        </button>
+
+        <button type="button" className="btn btn-outline-danger btn-small">
+          <i className="fa fa-trash-o"/>
+        </button>
+        </div>
     );
   };
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import AppHeader from './components/app-header';
+import ItemStatusFilter from './components/item-status-filte';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
 
@@ -9,14 +10,16 @@ import TodoList from './components/todo-list';
 const App = () => {
 
   const todoData =[
-    { label: 'Drink Cofee', important: false}, 
-    { label: 'Another text', important: true}, 
-    { label: 'Tttt test text', important: false} 
+    { label: 'Drink Cofee', important: false, id: 1}, 
+    { label: 'Another text', important: true, id: 2}, 
+    { label: 'Tttt test text', important: false, id: 3} 
   ]
   return (
     <div>
       <AppHeader />
+
       <SearchPanel />
+      <ItemStatusFilter/>
       <TodoList todos={ todoData }/>
     </div>
   );
