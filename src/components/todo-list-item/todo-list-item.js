6d1a48470fcf8a5607a08ae4dbf07c5bff8ebb10
Napolik <napolik@MacBook-MacBook.Dlink>
2021-07-22 11:29:43 +0300

    lesson 35 done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,13 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  constructor() {
+    super();
+    this.onLabelClick = () => {
+      console.log(`Done: ${ this.props.label }`);
+    };
+  }
+
   render() {
 
     const {label, important = false} = this.props;
@@ -14,8 +21,9 @@ export default class TodoListItem extends Component {
           <div>
           <span 
             className="todo-list-item" 
-            style={style}>
-            { label }
+            style={style}
+            onClick={ this.onLabelClick }>
+            { label }  
           </span>
   
           <button type="button" className="btn btn-outline-success btn-small">
