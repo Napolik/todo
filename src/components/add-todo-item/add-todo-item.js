a78081facc2f82bf70a3c790185b4d7a6cc05014
RomanVelboi <wellboychik@gmail.com>
2021-08-26 14:11:16 +0300

    lesson 44 done

--- src/components/add-todo-item/add-todo-item.js
+++ src/components/add-todo-item/add-todo-item.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import './add-todo-item.css';
 
 export default class AddTodoItem extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    }
+
     render() {
         return ( 
-            <div className="add-todo-item">
+            <form className="add-todo-item d-flex" onSubmit={this.onSubmit}>
+                
+                <input type="text" className="form-control"
+                onChange={this.onLabelChange}
+                placeholder="Enter text" />
             <button className="btn btn-outline-secondary"
-            onClick={() =>  this.props.onItemAdded('Hello') }
+            
             >Add</button>
-           </div>
+           </form>
         );
     }
   };
